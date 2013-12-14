4d1146be3a538ab7adfa80f204caa75d56adbaa0
Ales Komarek <mail@newt.cz>
2013-12-14 15:30:59 +0100

    Name fix

--- horizon_monitoring/checks/tables.py
+++ horizon_monitoring/checks/tables.py
@@ -4,7 +4,7 @@ from horizon import tables
 from horizon_monitoring.utils.tables import NoActionDataTable
 
 class SensuChecksTable(NoActionDataTable):
-    name = tables.Column('name', verbose_name=_("name"))
+    name = tables.Column('name', verbose_name=_("Name"))
     command = tables.Column('command', verbose_name=_("Command"))
     subscribers = tables.Column('subscribers', verbose_name=_("Subscribers"))
     interval = tables.Column('interval', verbose_name=_("Interval"))
@@ -12,3 +12,4 @@ class SensuChecksTable(NoActionDataTable):
     class Meta:
         name = "checks"
         verbose_name = _("Checks")
+
