7b165673a56bb23a30c59aff9836dce4cd1fa9af
Ales Komarek <mail@newt.cz>
2013-12-14 15:29:06 +0100

    imports fix

--- horizon_monitoring/checks/tables.py
+++ horizon_monitoring/checks/tables.py
@@ -1,8 +1,9 @@
 
 from django.utils.translation import ugettext_lazy as _
-from horizon_monitoring.utils import tables
+from horizon import tables
+from horizon_monitoring.utils.tables import NoActionDataTable
 
-class SensuChecksTable(tables.NoActionDataTable):
+class SensuChecksTable(NoActionDataTable):
     name = tables.Column('name', verbose_name=_("name"))
     command = tables.Column('command', verbose_name=_("Command"))
     subscribers = tables.Column('subscribers', verbose_name=_("Subscribers"))
