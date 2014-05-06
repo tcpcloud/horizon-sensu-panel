23b240070e473daf49645e8716144401da874962
Ales Komarek <mail@newt.cz>
2014-05-06 12:11:18 +0200

    correct ordering

--- horizon_monitoring/utils/sensu_client.py
+++ horizon_monitoring/utils/sensu_client.py
@@ -40,7 +40,7 @@ class Sensu(object):
     @property
     def event_list(self):
         events = self.request('/events')
-        return sorted(sorted(events, key=lambda x: x['status'], reverse=False), key=lambda x: x['client'], reverse=True)
+        return sorted(sorted(events, key=lambda x: x['client'], reverse=False), key=lambda x: x['status'], reverse=True)
         #return events
 
     def event_detail(self, check, client):
