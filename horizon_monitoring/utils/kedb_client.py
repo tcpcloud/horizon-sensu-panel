62389b4b21163570b01a8dd4ea88bb38197748e9
Ales Komarek <mail@newt.cz>
2014-05-19 15:30:59 +0200

    api preps

--- horizon_monitoring/utils/kedb_client.py
+++ horizon_monitoring/utils/kedb_client.py
@@ -27,4 +27,26 @@ class Kedb(object):
     def error_list(self):
         return self.request('/api/known-errors')
 
+    def check_detail(self, error):
+        url = '%s/known-errors/%s' % (self.api, error)
+        response = requests.get(url)
+        return response.json()
+
+    def error_create(self, error, data):
+        payload = data
+        url = '%s/known-errors' % self.api
+        response = requests.put(url, data=json.dumps(payload))
+        return response.json()
+
+    def error_update(self, error, data):
+        payload = data
+        url = '%s/known-errors/%s' % (self.api, error)
+        response = requests.post(url, data=json.dumps(payload))
+        return response.json()
+
+    def error_delete(self, error):
+        url = '%s/known-errors/%s' % (self.api, error)
+        response = requests.get(url)
+        return response.json()
+
 kedb_api = Kedb()
