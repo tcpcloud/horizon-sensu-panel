1610b53082460fc157c549ee27436bb5889aeb75
Michael Kuty <6du1ro.n@gmail.com>
2014-05-21 16:22:06 +0200

    simplicity

--- horizon_monitoring/utils/kedb_client.py
+++ horizon_monitoring/utils/kedb_client.py
@@ -15,43 +15,49 @@ class Kedb(object):
     def __init__(self):
         pass
 
-    def request(self, path):
-        request = requests.get('%s%s' % (self.api, path))
-        return request.json()
+    def request(self, path, method="GET", params={}):
+        log.debug("%s - %s - %s"%(method,path,params))
+
+        if method == "GET":
+            request = requests.get('%s%s' % (self.api, path))
+        elif method == "POST":
+            headers = {"Content-Type": "application/json" }
+            request = requests.post('%s%s' % (self.api, path),data=json.dumps(params),headers=headers)
+        
+        if request.status_code in (200, 201):
+            return request.json()
+        else:
+            """handle errors"""
+#            messages.error(request, "%s - %s - %s - %s" % (method, path, params, request.status_code))
+            return [{'id':1, 'name': request.status_code}, {'id':2, 'name': request.text}]
 
     @property
     def api(self):
-        return 'http://%s:%s' % (self.host, self.port)
+        return 'http://%s:%s/api' % (self.host, self.port)
 
     @property
     def workaround_list(self):
-        return self.request('/api/workarounds')
+        return self.request('/workarounds')
 
     @property
     def error_list(self):
-        return self.request('/api/known-errors')
+        return self.request('/known-errors')
 
     def workaround_detail(self, workaround):
-        url = '%s/api/workarounds/%s/' % (self.api, workaround)
-        response = requests.get(url)
-        return response.json()
+        url = '/workarounds/%s/' % (workaround)
+        return requests.get(url)
 
     def error_detail(self, error):
-        url = '%s/api/known-errors/%s/' % (self.api, error)
-        response = requests.get(url)
-        return response.json()
+        url = '/known-errors/%s/' % (error)
+        return self.request(url)
 
     def error_create(self, error, data):
-        payload = data
-        url = '%s/api/known-errors' % self.api
-        response = requests.put(url, data=json.dumps(payload))
-        return response.json()
+        url = '%s/known-errors' % self.api
+        return self.request(url, "PUT", data)
 
     def error_update(self, error, data):
-        payload = data
-        url = '%s/api/known-errors/%s/' % (self.api, error)
-        response = requests.put(url, data=json.dumps(payload))
-        return response.json()
+        url = '%s/known-errors/%s/' % (self.api, error)
+        return self.request(url, "POST", data)
 
     def error_delete(self, error):
         url = '%s/api/known-errors/%s' % (self.api, error)
