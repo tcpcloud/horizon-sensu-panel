784b9794dd8d9e389b1039fc1785826872015462
Michael Kuty <6du1ro.n@gmail.com>
2014-05-22 12:31:54 +0200

    fixes

--- horizon_monitoring/utils/request_util.py
+++ horizon_monitoring/utils/request_util.py
@@ -1,4 +1,4 @@
-    
+
 import requests 
 import json
 import logging
@@ -6,6 +6,61 @@ from horizon import messages
 from django.conf import settings
 
 log = logging.getLogger('utils.make_request')
+
+
+class Req(object):
+    """
+    """
+    
+    def __init__(self, *args, **kwargs):
+        super(Req, self).__init__(*args, **kwargs)
+
+    @staticmethod
+    def make_request(path, method="GET", params={}, request=None):
+        """small util method for simplify create request with handled exceptions
+        and debug output
+
+        .. attribute:: path
+
+            Required.
+
+        .. attribute:: method
+
+            The HTTP method for this action. Defaults to ``GET``. Other methods
+            may or may not succeed currently.
+
+        .. attribute:: params
+
+            Default to be an empty dictionary (``{}``)
+
+        .. attribute:: request
+
+            Django request object. Provides django messages.
+            Useful in debug. 
+
+        """
+        log.debug("%s - %s - %s"%(method,path,params))
+
+        if method == "GET":
+            response = requests.get(path)
+
+        elif method in ["POST", "PUT", "DELETE"]:
+            headers = {"Content-Type": "application/json" }
+            req = requests.Request(method, path, data=json.dumps(params),headers=headers).prepare()
+            response = requests.Session().send(req)
+
+        """delete ok"""
+        if response.status_code == 204:
+            return True
+
+        if response.status_code in (200, 201):
+            return response.json()
+        else:
+            if request:
+                """handle errors"""
+                messages.error(request, "%s - %s - %s - %s - %s" % (method, path, params, response.status_code, str(response.text)))
+                return {}
+            return { 'status_code': response.status_code, 'text': response.text }
 
 class BaseClient(object):
     """small util class for easy api manipulate
@@ -33,65 +88,7 @@ class BaseClient(object):
     protocol = "http"
     api_prefix = "api"
 
-    _req = None
-    
-    @property
-    def req(self):
-        if not self._req:
-            try:
-                self._req = Req(self.host, 
-                                self.port,
-                                self.protocol,
-                                self.api_prefix)
-            except Exception, e:
-                raise e
-        return  self._req
-
-    def request(self, *args, **kwargs):
-        if self.req:
-            return self.req.make_request(*args, **kwargs)
-        else:
-            #missing req util instance
-            return None
-
-    def __init__(self, *args, **kwargs):
-        super(BaseClient, self).__init__(*args, **kwargs)
-
-class Req(object):
-    """small util method for simplify create request with handled exceptions
-    and debug output
-
-    .. attribute:: host
-
-        Required.
-
-    .. attribute:: port
-
-        Required.
-
-    .. attribute:: api_prefix
-
-        Default to be a string (``api``)
-
-    .. attribute:: protocol
-
-        Optional.
-
-    """
-
-    host = None
-    port = None
-    port = "HTTP"
-    api_prefix = "api"
-    
-    def __init__(self, host, port, protocol="HTTP", api_prefix="api"):
-        try:
-            self.host = host
-            self.port = port
-            self.protocol = protocol
-            self.api_prefix = api_prefix
-        except Exception, e:
-            raise e
+    req = Req()
 
     @property
     def api(self, api_prefix="api"):
@@ -100,7 +97,7 @@ class Req(object):
                                             self.port,
                                             api_prefix)
 
-    def make_request(self, path, method="GET", params={}, request=None):
+    def request(self, *args, **kwargs):
         """small util method for simplify create request with handled exceptions
         and debug output
 
@@ -123,24 +120,25 @@ class Req(object):
             Useful in debug. 
 
         """
-        log.debug("%s - %s - %s"%(method,path,params))
+        url = args[0]
+        method = "GET"
+        data = {}
+        request = None
+        try:
+            method = args[1]
+            data = args[2]
+        except Exception, e:
+            pass
 
-        if method == "GET":
-            response = requests.get('%s%s' % (self.api, path))
-        elif method in ["POST", "PUT", "DELETE"]:
-            headers = {"Content-Type": "application/json" }
-            req = requests.Request(method, '%s%s' % (self.api, path),data=json.dumps(params),headers=headers).prepare()
-            response = requests.Session().send(req)
+        try:
+            request = args[3]
+        except Exception, e:
+            pass
 
-        """delete ok"""
-        if response.status_code == 204:
-            return True
+        if not url:
+            url = kwargs.get("url", "")
+        _url = "{0}{1}".format(self.api, url)
+        return self.req.make_request(path=_url,method=method, params=data, request=request)
 
-        if response.status_code in (200, 201):
-            return response.json()
-        else:
-            if request:
-                """handle errors"""
-                messages.error(request, "%s - %s - %s - %s - %s" % (method, path, params, response.status_code, str(response.text)))
-                return {}
-            return { 'status_code': response.status_code, 'text': response.text }
+    def __init__(self, *args, **kwargs):
+        super(BaseClient, self).__init__(*args, **kwargs)
