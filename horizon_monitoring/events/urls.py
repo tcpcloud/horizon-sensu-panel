25de692072f938cd703a5d7fea73a3b753a28fae
Ales Komarek <mail@newt.cz>
2014-05-23 08:23:44 +0200

    url map fix

--- horizon_monitoring/events/urls.py
+++ horizon_monitoring/events/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     url(r'^fullscreen/$', FullScreenIndexView.as_view(), name='fullscreen_index'),
     url(r'^events/(?P<check>[\w\_\.]+)/(?P<client>[\w\.\-]+)/detail/$', DetailView.as_view(), name='detail'),
     url(r'^events/(?P<check>[\w\_\.]+)/silence-check/$', SilenceView.as_view(), name='silence_check'),
-    url(r'^events/(?P<client>[\w\_\.]+)/silence-client/$', SilenceView.as_view(), name='silence_client'),
+    url(r'^events/(?P<client>[\w\-\_\.]+)/silence-client/$', SilenceView.as_view(), name='silence_client'),
     url(r'^events/(?P<check>[\w\_\.]+)/(?P<client>[\w\.\-]+)/silence-client-check/$', SilenceView.as_view(), name='silence_client_check'),
 )
