b5893a2cce9e823bbb437081229b8bf7130fbbb4
Michael Kuty <6du1ro.n@gmail.com>
2014-08-18 09:08:11 +0200

    fix url params

--- horizon_monitoring/checks/urls.py
+++ horizon_monitoring/checks/urls.py
@@ -4,5 +4,5 @@ from .views import IndexView, RequestView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
-    url(r'^(?P<check>[\w\_\.]+)/request/$', RequestView.as_view(), name='request'),
+    url(r'^(?P<check>[\w\-\_\.]+)/request/$', RequestView.as_view(), name='request'),
 )
