1876f53f7ebf6fd1cec9c84353073ef89b9fc300
Michael Kutý <6du1ro.n@gmail.com>
2015-03-17 13:33:36 +0100

    rewrite some views with new approach, models with managers

---
+++ horizon_monitoring/errors/managers.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+
+from horizon_monitoring.utils.kedb_client import KedbManager
+
+
+class Manager(KedbManager):
+
+    def all(self, *args, **kwargs):
+        return self.request('/known-errors')
+
+    def get(self, id):
+
+        return self.update(id)
+
+    def update(self, error, data=None):
+        url = '/known-errors/%s/' % (error)
+        if not data:
+            return self.request(url)
+        return self.request(url, "PUT", data)
+
+    def create(self, data, request=None):
+        url = '/known-errors/'
+        return self.request(url, "POST", data, request)
+
+    def delete(self, request, error):
+        url = '/known-errors/%s/' % (error)
+        return self.request(url, "DELETE", {}, request)
